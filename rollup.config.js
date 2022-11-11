9fd618c6ffbf1858172de991b4f736b0fe7d410d
kiki-kanri <a470666@gmail.com>
2022-11-12 07:29:17 +0800

    Add and setting rollup and other packages

---
+++ rollup.config.js
@@ -0,0 +1,34 @@
+import { defineConfig } from 'rollup';
+import babel from '@rollup/plugin-babel';
+import commonjs from '@rollup/plugin-commonjs';
+import path from 'path';
+import resolve from '@rollup/plugin-node-resolve';
+import strip from '@rollup/plugin-strip';
+import terser from '@rollup/plugin-terser';
+import typescript from '@rollup/plugin-typescript';
+
+const srcPath = path.resolve(__dirname, 'src', 'index.ts');
+const distPath = path.resolve(__dirname, 'dist');
+const production = !process.env.ROLLUP_WATCH;
+
+export default defineConfig({
+	input: srcPath,
+	output: {
+		dir: distPath,
+		name: 'index.js'
+	},
+	plugins: [
+		babel(),
+		commonjs(),
+		resolve(),
+		strip({
+			include: ['**/*.ts']
+		}),
+		production && terser({
+			module: true
+		}),
+		typescript({
+			module: 'esnext'
+		})
+	]
+});
