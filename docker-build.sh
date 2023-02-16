b480c27bab60ca64f1ad8fb0d64942f900e35898
kiki-kanri <a470666@gmail.com>
2023-02-16 14:34:50 +0800

    Edit dockerfile and scripts.

--- docker-build.sh
+++ docker-build.sh
@@ -1,2 +1,14 @@
-#!/bin/sh
-docker build -t kiki-kanri/nodets-template:latest .
+#!/bin/bash
+
+name=""
+user=""
+
+docker build -t $user/$name:latest . || exit
+[ "$(docker ps | grep $name)" ] && docker kill $name
+[ "$(docker ps -a | grep $name)" ] && docker rm $name
+
+# docker run \
+# 	-itd \
+# 	--name $name \
+# 	--restart=always \
+# 	$user/$name:latest
