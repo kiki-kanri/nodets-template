21e6c1e9d449a1bdaaef0fc3d58f53a985d50066
kiki-kanri <a470666@gmail.com>
2023-08-03 19:27:26 +0800

    Edit esm loader, fixed some import path not work error.

--- esm-loader.js
+++ esm-loader.js
@@ -1,12 +1,38 @@
-import * as tsConfigPaths from 'tsconfig-paths';
+import fs from 'node:fs';
+import { dirname, join, resolve as resolvePath } from 'path';
+import { createMatchPath, loadConfig } from 'tsconfig-paths';
 import { getFormat, load, resolve as resolveTs, transformSource } from 'ts-node/esm';
+import { pathToFileURL } from 'url';
 export { getFormat, load, transformSource };
 
-const { absoluteBaseUrl, paths } = tsConfigPaths.loadConfig();
-const matchPath = tsConfigPaths.createMatchPath(absoluteBaseUrl, paths);
+const { absoluteBaseUrl, paths } = loadConfig();
+const matchPath = createMatchPath(absoluteBaseUrl, paths);
+
+/**
+ * @param {string} path
+ */
+function isdir(path) {
+	try {
+		return fs.statSync(path).isDirectory();
+	} catch (error) { }
+	return false;
+}
 
 export function resolve(specifier, context, defaultResolver) {
-	const mappedSpecifier = matchPath(specifier);
-	if (mappedSpecifier) specifier = `${mappedSpecifier}.ts`;
+	let mappedSpecifier = matchPath(specifier);
+	if (!mappedSpecifier && specifier.startsWith('.')) {
+		const baseUrl = context.parentURL || pathToFileURL('./src');
+		const basePath = resolvePath(baseUrl.replace(/file:\/\//, ''));
+		mappedSpecifier = join(dirname(basePath), specifier);
+	}
+
+	if (mappedSpecifier) {
+		if (isdir(mappedSpecifier)) {
+			specifier = join(mappedSpecifier, 'index.ts');
+		} else if (!mappedSpecifier.match(/\.(j|t)s$/)) {
+			specifier = `${mappedSpecifier}.ts`;
+		}
+	}
+
 	return resolveTs(specifier, context, defaultResolver);
 }
