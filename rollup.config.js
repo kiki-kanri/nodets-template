5282d367b60c14977acb74e9357724e7a85ab836
kiki-kanri <a470666@gmail.com>
2023-08-23 19:23:52 +0800

    chore: edit rollup config comment

--- rollup.config.js
+++ rollup.config.js
@@ -9,6 +9,7 @@ const distPath = path.join(__dirname, 'dist');
 const esbuild = _esbuild.default || _esbuild;
 const inputPath = path.join(__dirname, 'src', 'index.ts');
 
+// https://rollupjs.org/configuration-options
 export default defineConfig({
 	// Use this setting to set the package as an external package, such as fs-extra, lodash...etc.
 	// Docs: https://rollupjs.org/configuration-options/#external
@@ -20,7 +21,7 @@ export default defineConfig({
 		format: 'cjs'
 	},
 	plugins: [
-		// Must remove debugger statements before other plugins.
+		// Remove the debugger statement plugin must be loaded before the rest of the plugin with transform method.
 		strip({ include: ['./src/**/*.ts'] }),
 		tsConfigPaths(),
 		esbuild({ minify: true }),
