48d24e9d295481191b17026a5f16bd08a669aa30
kiki-kanri <a470666@gmail.com>
2023-08-23 13:39:09 +0800

    build: change plugins, edit config and build script

--- rollup.config.js
+++ rollup.config.js
@@ -1,29 +1,30 @@
-import strip from '@rollup/plugin-strip';
 import path from 'path';
 import { defineConfig } from 'rollup';
-import _esbuild from 'rollup-plugin-esbuild';
+import del from 'rollup-plugin-delete';
 import externals from 'rollup-plugin-node-externals';
-import tsConfigPaths from 'rollup-plugin-tsconfig-paths';
+import ts from 'rollup-plugin-ts';
 
 const distPath = path.join(__dirname, 'dist');
-const esbuild = _esbuild.default || _esbuild;
 const inputPath = path.join(__dirname, 'src', 'index.ts');
 
+// https://rollupjs.org/configuration-options
 export default defineConfig({
-	// Use this setting to set the package as an external package, such as fs-extra, lodash...etc.
-	// Docs: https://rollupjs.org/configuration-options/#external
-	external: [],
 	input: inputPath,
 	output: {
 		dir: distPath,
-		name: 'index.js',
-		format: 'cjs'
+		exports: 'named',
+		format: 'cjs',
+		generatedCode: {
+			arrowFunctions: true,
+			constBindings: true,
+			objectShorthand: true
+		},
+		preserveModules: true,
+		validate: true
 	},
 	plugins: [
-		// Must remove debugger statements before other plugins.
-		strip({ include: ['./src/**/*.ts'] }),
-		tsConfigPaths(),
-		esbuild({ minify: true }),
-		externals()
+		del({ target: './dist' }),
+		externals(),
+		ts()
 	]
 });
